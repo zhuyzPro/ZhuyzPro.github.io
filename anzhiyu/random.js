41010f4fb3a060234100747a53ae25c7bfab75f2
Zhuyz Pro <zhuyzPro@users.noreply.github.com>
2026-07-11 20:08:41 +0800

    Site updated: 2026-07-11 20:08:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/07/11/blog-setup/","2026/07/11/project-roadmap/","2026/07/11/github-pages-domain/","2026/07/11/theme-notes/"];function toRandomPost(){
+var posts=["/posts/e140.html","/posts/136a.html","/posts/8105.html","/posts/340b.html","/posts/b228.html","/posts/40fd.html","/posts/4eb4.html","/posts/dfb3.html","/posts/d335.html","/posts/a76e.html","/posts/39a9.html","/posts/2594.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
