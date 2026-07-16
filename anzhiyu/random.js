a92a2d8e17afb121faa3c0c9a9b4e39694795ad7
Zhuyz Pro <zhuyzPro@users.noreply.github.com>
2026-07-16 22:51:13 +0800

    Site updated: 2026-07-16 22:51:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/07/16/project-notes-template/","2026/07/16/writing-template/","2026/07/16/site-initialized/"];function toRandomPost(){
+var posts=["2026/07/16/project-notes-template/","2026/07/16/site-initialized/","2026/07/16/writing-template/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
